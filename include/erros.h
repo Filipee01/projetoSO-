b62e5c044b449d124fb6f41664ae0d4fd17ce90c
Souza <luiz.a.souza@accenture.com>
2025-09-20 18:25:09 -0300

    feat: add errors emulation

---
+++ include/erros.h
@@ -0,0 +1,13 @@
+// erros.h
+#ifndef ERROS_H
+#define ERROS_H
+
+// funções para emular erros
+void trigger_stackoverflow();
+void trigger_segfault();
+void trigger_buffer_overflow();
+void trigger_memory_leak();
+void trigger_race_condition();
+void trigger_deadlock();
+
+#endif
