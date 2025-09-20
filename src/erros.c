1179cc11e2ae035219ef743ae9a82a8df6f2bd68
Souza <luiz.a.souza@accenture.com>
2025-09-20 17:59:09 -0300

    feat: add stack overflow and segmentation fault

---
+++ src/erros.c
@@ -0,0 +1,25 @@
+// erros.c
+#include "erros.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <pthread.h>
+#include <unistd.h>  // Para a função sleep()
+
+
+// ==========================
+// 1. STACK OVERFLOW
+// ==========================
+// Stack overflow ocorre quando a pilha de execução enche.
+// Aqui simulamos isso com recursão infinita: cada chamada ocupa espaço na pilha
+// até ela "explodir".
+
+
+void trigger_stackoverflow() {
+ printf("Stack overflow em andamento...\n");
+ trigger_stackoverflow(); // recursão infinita até estourar a pilha 
+ }
+
+ 
+
+
